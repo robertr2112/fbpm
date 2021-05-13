fe2e80e30e5e9d19fae13fe0ee266ac2276fad07
Robert Reynolds <robertr2112@yahoo.com>
2021-05-13 18:30:59 -0500

    Added admin_mgmt nav-tabs and Teams controls

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,4 @@
 class StaticPagesController < ApplicationController
-  def home
+  def admin_mgmt
   end
 end
--- app/models/season.rb
+++ app/models/season.rb
@@ -13,6 +13,12 @@
 #
 class Season < ApplicationRecord
 
+  before_create do
+    self.year = Time.now.strftime("%Y")
+    self.current_week = 1
+    self.state = Season::STATES[:Pend]
+  end
+
   has_many :weeks, dependent: :delete_all
 
   accepts_nested_attributes_for :weeks, allow_destroy: true
@@ -25,8 +31,56 @@ class Season < ApplicationRecord
     return year.to_s
   end
 
+  def setState(new_state)
+    self.update_attribute(:state, new_state)
+  end
+
+  def isPending?
+    self.state == STATES[:Pend]
+  end
+
+  def isOpen?
+    self.state == STATES[:Open]
+  end
+
+  def isClosed?
+    self.state == STATES[:Closed]
+  end
+
+  def canBeClosed?
+    if self.weeks.empty?
+      return false
+    end
+    if self.weeks.count != self.number_of_weeks
+      return false
+    end
+    self.weeks.order(:week_number).each do |week|
+      if !week.checkStateFinal
+        return false
+      end
+    end
+    return true
+  end
+
+  #
+  # Return a link to the current_week record
+  #
   def getCurrentWeek
     self.weeks.find_by_week_number(self.current_week)
   end
 
+  #
+  # Have every pool update their entries. This is called after a week is marked 'final'
+  #
+  def updatePools
+    self.pools.each do |pool|
+      if self.getCurrentWeek.week_number >= pool.starting_week
+        pool.updateEntries(self.getCurrentWeek)
+      end
+    end
+    if self.current_week < self.number_of_weeks
+      self.update_attribute(:current_week, self.current_week+1)
+    end
+  end
+  
 end
