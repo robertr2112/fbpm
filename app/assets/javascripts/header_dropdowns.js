de2bc26be4e39b43ba0a34bb01cac24ae978f01e
Robert Reynolds <robertr2112@yahoo.com>
2021-10-17 16:24:40 -0500

    Updated header_dropdowns.js

--- app/assets/javascripts/header_dropdowns.js
+++ app/assets/javascripts/header_dropdowns.js
@@ -1,23 +1,21 @@
 // jquery ready start
-//$(document).ready(function() {
+$(document).ready(function() {
 	// jQuery code
 
 	//////////////////////// Prevent closing from click inside dropdown
- //   $(document).on('click', '.dropdown-menu', function (e) {
-  //    e.stopPropagation();
-   // });
+  $(document).on('click', '.dropdown-menu', function (e) {
+    e.stopPropagation();
+  });
 
     //  make it as accordion for smaller screens
-//  if ($(window).width() < 576) {
-//	  	$('.dropdown-menu a').click(function(e){
-//	  		e.preventDefault();
-//	        if($(this).next('.submenu').length){
-//	        	$(this).next('.submenu').toggle();
-//	        }
-//	        $('.dropdown').on('hide.bs.dropdown', function () {
-//			   $(this).find('.submenu').hide();
-//			})
-//	  });
-//	}
+    if ($(window).width() < 768) {
+  	  $('.dropdown-menu a').click(function(e){
+  	  	e.preventDefault();
+  	    if($(this).next('.submenu').length){
+  	     	$(this).next('.submenu').toggle();
+  	     	$(this).getElementByClass('.submenu-left').style.display = "block";
+  	    }
+  	  });
+  	}
 
-//}); // jquery end
+}); // jquery end
