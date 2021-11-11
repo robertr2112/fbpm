1de45b648a56a628c6f137d4d09f4b5dfe676bbd
Robert Reynolds <robertr2112@yahoo.com>
2021-11-11 09:33:01 -0600

    Updating assets for new multi-level dropdowns

--- public/assets/application-d299dc6b716a95b7b57179fb7ca61449a77d4a1a24d41e272da00354fd713d34.js
+++ public/assets/application-d299dc6b716a95b7b57179fb7ca61449a77d4a1a24d41e272da00354fd713d34.js
@@ -24378,27 +24378,60 @@ function setSelect2Format() {
   });
 
 };
-// jquery ready start
-//$(document).ready(function() {
-	// jQuery code
+(function($) {
+    var defaults={
+        sm : 540,
+        md : 720,
+        lg : 960,
+        xl : 1140,
+        navbar_expand: 'lg',
+        animation: true,
+        animateIn: 'fadeIn',
+    };
+    $.fn.bootnavbar = function(options) {
 
+        var screen_width = $(document).width();
+        settings = $.extend(defaults, options);
+
+        if(screen_width >= settings.lg){
+            $(this).find('.dropdown').hover(function() {
+                $(this).addClass('show');
+                $(this).find('.dropdown-menu').first().addClass('show');
+                if(settings.animation){
+                    $(this).find('.dropdown-menu').first().addClass('animated ' + settings.animateIn);
+                }
+            }, function() {
+                $(this).removeClass('show');
+                $(this).find('.dropdown-menu').first().removeClass('show');
+            });
+        }
+
+        $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+          if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+          }
+          var $subMenu = $(this).next(".dropdown-menu");
+          $subMenu.toggleClass('show');
+
+          $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+            $('.dropdown-submenu .show').removeClass("show");
+          });
+
+          return false;
+        });
+    };
+})(jQuery);
+
+// jquery ready start
+$(document).ready(function() {
+	// jQuery code
 	//////////////////////// Prevent closing from click inside dropdown
 // $(document).on('click', '.dropdown-menu', function (e) {
 //    e.stopPropagation();
 //  });
+  $('#main_navbar').bootnavbar();
 
-    //  make it as accordion for smaller screens
-//    if ($(window).width() < 768) {
-// 	  $('.dropdown-menu a').click(function(e){
-//	  	e.preventDefault();
-// 	    if($(this).next('.submenu').length){
-//	     	$(this).next('.submenu').toggle();
-// 	     	$(this).getElementByClass('.submenu-left').style.display = "block";
-//  	    }
-// 	  });
-//  	}
-
-//}); // jquery end;
+}); // jquery end;
 // Use select2 for picking teams
 //$(document).ready(function() { $("#gamePick").select2(); });
 // This is to get the weeks dropdown to not hide under the header when opened.
