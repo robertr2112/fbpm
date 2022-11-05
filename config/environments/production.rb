f645e6e2cde1446d5b648ec41c34a0c4e55b19f4
Robert Reynolds <robertr2112@yahoo.com>
2022-11-05 09:54:34 -0500

    updating production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,7 @@ Rails.application.configure do
       authentication:       :plain,
       user_name:            'apikey',
       password:             ENV['SENDGRID_API_KEY'],
-      domain:               'heroku.com',
-      enable_starttls_auto: true
+      domain:               'heroku.com'
   }
 
   # Use a different logger for distributed setups.
