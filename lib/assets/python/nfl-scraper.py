a9445bc828d744269ee9bd6baf5040c8a6be9644
Robert Reynolds <robertr2112@yahoo.com>
2023-09-28 16:34:35 -0500

    Adding --disable-dev-shm-usage argument to chrome options

--- lib/assets/python/nfl-scraper.py
+++ lib/assets/python/nfl-scraper.py
@@ -143,6 +143,7 @@ week = arguments['weekNumber']
 uc_options = uc.ChromeOptions()
 uc_options.add_argument('--headless')
 uc_options.add_argument('--no-sandbox')
+uc_options.add_argument('--disable-dev-shm-usage')
 driver = uc.Chrome(options=uc_options, use_subprocess=False)
 
 #driver = uc.Chrome(headless=True,use_subprocess=False)
