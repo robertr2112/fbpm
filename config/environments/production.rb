857c95bf4f09ace3dda5a06e6ef450c57b9c221f
Robert Reynolds <robertr2112@yahoo.com>
2025-09-23 14:19:43 -0500

    Disabled YJIT compiler to ease memory issues on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -53,19 +53,18 @@ Rails.application.configure do
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
-
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
+                                       .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+                                       .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
   # want to log everything, set the level to "debug".
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -93,7 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Only use :id for inspections in production.
-  config.active_record.attributes_for_inspect = [ :id ]
+  config.active_record.attributes_for_inspect = [:id]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
@@ -104,14 +103,19 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   # Setup mailer
-  config.action_mailer.default_url_options = { host: "www.fbpm.club" }
+  config.action_mailer.default_url_options = { host: 'www.fbpm.club' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-      address:              "smtp.sendgrid.net",
-      port:                 "587",
-      authentication:       :plain,
-      user_name:            "apikey",
-      password:             ENV["SENDGRID_API_KEY"],
-      domain:               "fbpm.club"
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
+    domain: 'fbpm.club'
   }
+
+  # Disable YJIT compiler. Heroku ECO dyno running out of memory. If
+  # the app is moved to a different environment reassess this config
+  # option.
+  config.yjit = false
 end
